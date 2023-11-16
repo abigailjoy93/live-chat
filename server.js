162aed643f47a47fe3de53b66135b326b553c7d9
Abigail Peloquin <abigail.turnwall@gmail.com>
2023-11-15 20:39:57 -0600

    set up socket.io connection

--- server.js
+++ server.js
@@ -1,13 +1,31 @@
 const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
 const mongoose = require("./mongoose");
 
 const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
 const port = process.env.PORT || 3000;
 
-app.get("/api/greeting", (req, res) => {
-  res.json({ message: "Express API established" });
+// socket io event handling
+io.on("connection", (socket) => {
+  console.log("A user connected");
+
+  socket.on("disconnect", () => {
+    console.log("User disconnected");
+  });
 });
 
-app.listen(port, () => {
-  console.log(`Server is listening on port ${port}`);
+server.listen(port, () => {
+  console.log(`Server listening on port ${port}`);
 });
+
+// app.get("/api/greeting", (req, res) => {
+//   res.json({ message: "Express API established" });
+// });
+
+// app.listen(port, () => {
+//   console.log(`Server is listening on port ${port}`);
+// });
