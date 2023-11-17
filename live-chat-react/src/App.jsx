75849d64702e4b105bb8111c050d7103d554ca5b
David <131110984+Dtrieu1@users.noreply.github.com>
2023-11-16 21:20:08 -0600

    update react file structure

--- live-chat-react/src/App.jsx
+++ live-chat-react/src/App.jsx
@@ -3,6 +3,8 @@ import Footer from './components/Footer';
 function App() {
     return (
         <>
+            <main className='mx-3'></main>
+            <Footer />
         </>
     );
 }
--- live-chat-react/src/main.jsx
+++ live-chat-react/src/main.jsx
@@ -1,3 +1,5 @@
+import ReactDOM from 'react-dom/client'
+
 import App from './App';
 import Chatroom from './pages/Chatroom';
 import Home from './pages/Home';
