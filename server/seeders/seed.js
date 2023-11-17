2e81ec666dcda7f1aac669f70548e56f79c20f24
PengasKhan <cheesepuff42@gmail.com>
2023-11-16 21:20:48 -0600

    Seeds

---
+++ server/seeders/seed.js
@@ -0,0 +1,13 @@
+const db = require("../config/connection");
+const { User } = require("../models");
+const userSeeds = require("./userSeeds.json");
+const cleanDB = require("./cleanDB");
+
+db.once("open", async () => {
+  await cleanDB("User", "users");
+
+  await User.create(userSeeds);
+
+  console.log("all done!");
+  process.exit(0);
+});
