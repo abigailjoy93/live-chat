9134e4feaebf030735b519e7a2fcb9064e97daae
PengasKhan <cheesepuff42@gmail.com>
2023-11-16 19:12:10 -0600

    Backend file directory
    not much

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
-const mongoose = require("./mongoose");
+const mongoose = require("./config/mongoose");
 
 const { createServer } = require('node:http');
 
