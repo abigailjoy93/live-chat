e97585ad990ac27307aed864c968ff86abff9b43
Annie <anniebui0110@gmil.com>
2023-11-16 21:17:53 -0600

    schemas resolsver & typedefs

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,23 @@
+const typeDefs = `
+type User {
+    _id: ID
+    username: String
+    email: String
+    password: String
+}
+
+type Auth {
+    token: ID!
+    user: User
+}
+type Query {
+    users: [User]
+    user(username:String): User
+}
+type Mutation {
+    addUser(username: String!, email: String!, password: String!): Auth
+    login(email:String!, password: String!):Auth
+}
+`;
+
+module.exports = typeDefs;
