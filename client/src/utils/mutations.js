51c37d0ac6b5db0d677f32418142f6880d88593f
elthell <eleanor.thell@gmail.com>
2023-11-18 16:33:17 -0600

    add update / delete to client mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,4 +1,4 @@
-import { gql } from '@apollo/client';
+import { gql } from "@apollo/client";
 
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
@@ -19,6 +19,41 @@ export const ADD_USER = gql`
       user {
         _id
         username
+      }
+    }
+  }
+`;
+
+export const UPDATE_USER_NAME = gql`
+  mutation updateUserName($_id: ID!, $username: String!) {
+    updateUserName(_id: $_id, username: $username) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const UPDATE_USER_EMAIL = gql`
+  mutation updateUserName($_id: ID!, $email: String!) {
+    updateUserName(_id: $_id, email: $email) {
+      token
+      user {
+        _id
+        email
+      }
+    }
+  }
+`;
+
+export const DELETE_USER = gql`
+  mutation deleteUser($_id: ID!) {
+    deleteUser(_id: $_id) {
+      token
+      user {
+        _id
       }
     }
   }
