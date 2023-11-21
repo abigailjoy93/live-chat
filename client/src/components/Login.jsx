30579edf94945bf98438823678916225a983b149
PengasKhan <cheesepuff42@gmail.com>
2023-11-20 20:29:23 -0600

    Login HTML -> login component

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -38,7 +38,42 @@ const Login = (props) => {
     });
   };
 
-  return //<-- page here;;
+  return (
+    <main className="flex-row justify-center mb-">
+      <div className="col-12 col-lg-10">
+        <div className="card">
+          <h4 className="card-header bg-dark text-light p-2">Login</h4>
+          <div className="card-body">
+            <form onSubmit={handleFormSubmit}>
+              <input
+                className="form-input"
+                placeholder="Your email"
+                name="email"
+                type="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <input
+                className="form-input"
+                placeholder="******"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <button
+                className="btn btn-block btn-primary"
+                style={{ cursor: "pointer" }}
+                type="submit"
+              >
+                Submit
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default Login;
