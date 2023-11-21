d4d55f7eac6d20bb3bdf9502560fc4c57c51afbe
Annie <buixx185@gmail.com>
2023-11-20 21:29:06 -0600

    created a socket.js

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -9,6 +9,51 @@ import { setContext } from '@apollo/client/link/context';
 import { Outlet } from 'react-router-dom';
 import Footer from './components/Footer';
 
+// for socket.io
+import React, { useState, useEffect } from 'react';
+import { socket } from './socket';
+import { ConnectionState } from './components/ConnectionState';
+import { ConnectionManager } from './components/ConnectionManager';
+import { Events } from "./components/Events";
+import { MyForm } from './components/MyForm';
+
+export default function App() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [fooEvents, setFooEvents] = useState([]);
+
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    function onFooEvent(value) {
+      setFooEvents(previous => [...previous, value]);
+    }
+
+    socket.on('connect', onConnect);
+    socket.on('disconnect', onDisconnect);
+    socket.on('foo', onFooEvent);
+
+    return () => {
+      socket.off('connect', onConnect);
+      socket.off('disconnect', onDisconnect);
+      socket.off('foo', onFooEvent);
+    };
+  }, []);
+
+  return (
+    <div className="App">
+      <ConnectionState isConnected={ isConnected } />
+      <Events events={ fooEvents } />
+      <ConnectionManager />
+      <MyForm />
+    </div>
+  );
+}
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
   uri: '/graphql',
--- client/src/pages/Chatroom.jsx
+++ client/src/pages/Chatroom.jsx
@@ -3,7 +3,9 @@ import React from 'react';
 
 export default function Chatroom() {
     return (
-        <></>
+        <main>
+            
+        </main>
     );
 
 }
