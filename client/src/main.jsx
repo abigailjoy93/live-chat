1a76bc18038aee3533681901466787849bc85ac4
David <131110984+Dtrieu1@users.noreply.github.com>
2023-11-20 20:43:57 -0600

    update main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -7,13 +7,38 @@ import Home from './pages/Home.jsx';
 import Join from './pages/Join.jsx';
 import Chatroom from './pages/Chatroom.jsx';
 import Profile from './pages/Profile.jsx';
-import ErrorPage from './pages/ErrorPage.jsx'
+import ErrorPage from './pages/ErrorPage.jsx';
 
 const router = createBrowserRouter([
     {
         path: "/",
-        element: <App />
+        element: <App />,
         errorElement: <ErrorPage />,
-        child
-    }
-])
+        children: [
+            {
+                index: true,
+                element: <Home />
+            }, {
+                path: '/login',
+                element: <Login />
+            }, {
+                path: '/signup',
+                element: <Signup />
+            }, {
+                path: '/profiles/:username',
+                element: <Profile />
+            }, {
+                path: '/me',
+                element: <Profile />
+            }, {
+                path: '/thoughts/:thoughtId',
+                element: <SingleThought />
+            }
+        ]
+    },
+]);
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+    <RouterProvider router={router} />
+)
