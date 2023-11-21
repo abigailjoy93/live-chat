b5874b26ce52013e9094a5d406f7bd02003065c9
David <131110984+Dtrieu1@users.noreply.github.com>
2023-11-20 19:05:30 -0600

    update footer jsx structure

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+// import logo up here
+// import css if need be 
+
+function Footer() {
+    return (
+        <footer className=" footer text-center text-lg-start">
+
+            <div className="container d-flex justify-content-center">
+                {/* what ever content we want to add here */}
+            </div>
+        </footer>
+    );
+}
