187217ad3ff2bd47d7b34435231307c585cab8c5
PengasKhan <cheesepuff42@gmail.com>
2023-11-22 19:38:39 -0600

    Updated Form components

--- client/src/components/Login.jsx
+++ client/src/components/Login.jsx
@@ -1,7 +1,8 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import { useMutation } from "@apollo/client";
 import { LOGIN_USER } from "../utils/mutations";
 import Auth from "../utils/auth";
+import "./form.css";
 
 const Login = (props) => {
   const [formState, setFormState] = useState({ email: "", password: "" });
@@ -39,124 +40,53 @@ const Login = (props) => {
   };
 
   return (
-    <main
-      style={{
-        display: "flex",
-        flexDirection: "row",
-        justifyContent: "center",
-        marginBottom: "var(--margin-bottom)",
-      }}
-    >
-      <div
-        style={{
-          width: "100%",
-          maxWidth: "65rem",
-        }}
-      >
-        <div
-          style={{
-            border: "5px solid var(--accent)",
-            marginLeft: "25px",
-            marginRight: "25px",
-            marginBottom: "15px",
-            marginTop: "5px",
-            backgroundColor: "var(--light)",
-          }}
-        >
-          <h4
-            style={{
-              backgroundColor: "var(--accent)",
-              color: "var(--pop)",
-              padding: "0.5rem",
-              fontSize: "1.5rem",
-            }}
-          >
-            Log On:
-          </h4>
-          <div style={{ padding: "1rem" }}>
-            <form onSubmit={handleFormSubmit}>
-              <div>
-                <label
-                  style={{
-                    fontSize: "1.2rem",
-                  }}
-                  htmlFor="box"
-                >
-                  Log On:
+    <main className="container">
+      <div className="box">
+        <h4 className="box-title">Log On:</h4>
+        <div className="form">
+          <form onSubmit={handleFormSubmit}>
+            <div className="form-group">
+              <label htmlFor="box" className="box-label">
+                Log On:
+              </label>
+              <br />
+              <label htmlFor="email" className="email-label">
+                Email address:
+              </label>
+              <br />
+              <input
+                className="form-control"
+                placeholder="Your email"
+                name="email"
+                type="email"
+                value={formState.email}
+                onChange={handleChange}
+              />
+              <br />
+              <label htmlFor="pwd" className="password-label">
+                Password:
+              </label>
+              <br />
+              <input
+                className="form-control"
+                placeholder="******"
+                name="password"
+                type="password"
+                value={formState.password}
+                onChange={handleChange}
+              />
+              <div className="form-group">
+                <br />
+                <label className="remember">
+                  <input type="checkbox" /> Remember me
                 </label>
-                <br />
-                <label
-                  style={{
-                    fontSize: "1rem",
-                  }}
-                  htmlFor="email"
-                >
-                  Email address:
-                </label>
-                <br />
-                <input
-                  style={{
-                    height: "28px",
-                    width: "30vw",
-                    alignItems: "center",
-                    border: "2px solid var(--pop)",
-                  }}
-                  placeholder="Your email"
-                  name="email"
-                  type="email"
-                  value={formState.email}
-                  onChange={handleChange}
-                />
-                <br />
-                <label
-                  style={{
-                    fontSize: "1rem",
-                  }}
-                  htmlFor="pwd"
-                >
-                  Password:
-                </label>
-                <br />
-                <input
-                  style={{
-                    height: "28px",
-                    width: "30vw",
-                    alignItems: "center",
-                    border: "2px solid var(--pop)",
-                  }}
-                  placeholder="******"
-                  name="password"
-                  type="password"
-                  value={formState.password}
-                  onChange={handleChange}
-                />
-                <div>
-                  <br />
-                  <label
-                    style={{
-                      fontSize: "13px",
-                      fontWeight: "bold",
-                    }}
-                    className="remember"
-                  >
-                    <input type="checkbox" /> Remember me
-                  </label>
-                </div>
-                <br />
-                <button
-                  style={{
-                    height: "35px",
-                    width: "100px",
-                    fontSize: "15px",
-                    cursor: "pointer",
-                  }}
-                  type="submit"
-                >
-                  Login
-                </button>
               </div>
-            </form>
-          </div>
+              <br />
+              <button type="submit" className="btn btn-primary">
+                Login
+              </button>
+            </div>
+          </form>
         </div>
       </div>
     </main>
