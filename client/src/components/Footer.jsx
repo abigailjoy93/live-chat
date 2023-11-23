7e07bb59796af991bc524b81db86c2df5cd8273f
David <131110984+Dtrieu1@users.noreply.github.com>
2023-11-22 22:16:27 -0600

    update search

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -1,13 +1,18 @@
 import React from "react";
-import logoTransparent from "../assets/new-live-chat-logo.png";
+import logoTransparent from "../assets/astronaut-logo.png";
 function Footer() {
-    return (
-        <footer className=" footer text-center text-lg-start">
-            <div className="container d-flex justify-content-center">
-                <img class="logo" src={logoTransparent} alt="live chat logo"></img>
+  return (
+    <footer className=" footer text-center text-lg-start">
+      <div className="container d-flex justify-content-center">
+        <footer>
+          <div>
+            <div>
+              <img class="logo" src={logoTransparent} alt="live chat logo"></img>
             </div>
+          </div>
         </footer>
-    );
+      </div>
+    </footer>
+  );
 }
-
-export default Footer; 
+export default Footer;
